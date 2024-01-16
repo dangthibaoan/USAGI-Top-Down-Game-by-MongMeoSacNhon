47dc382acfd6017cbb2b6ccdd268046ee1e4e39c
dangthibaoan <baoan.ayako69@gmail.com>
2024-01-16 23:50:59 +0700

    add singeton to HomeController

--- Assets/_Root/Scripts/Controller/HomeController.cs
+++ Assets/_Root/Scripts/Controller/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 
-public class HomeController : MonoBehaviour
+public class HomeController : Singleton<HomeController>
 {
     void Start()
     {
