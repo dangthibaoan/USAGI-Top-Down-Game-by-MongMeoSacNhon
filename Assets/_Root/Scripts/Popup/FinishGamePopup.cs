d65932f2dfc36a0ab7797ef6e7d89ca4fe8c67fa
dangthibaoan <baoan.ayako69@gmail.com>
2024-01-18 23:03:38 +0700

    update FinishGamePopup

--- Assets/_Root/Scripts/Popup/FinishGamePopup.cs
+++ Assets/_Root/Scripts/Popup/FinishGamePopup.cs
@@ -6,6 +6,37 @@ using UnityEngine;
 
 public class FinishGamePopup : Popup
 {
-    [SerializeField] private Image ImageCongrats;
+    [SerializeField] private GameObject ImageCongrats;
+    [SerializeField] private GameObject ImageNewRecord;
     [SerializeField] private TMP_Text CurrentScore, MaxScore;
+    [SerializeField] private SoundType FinishMusic;
+
+    private void Awake()
+    {
+        this.ImageNewRecord.SetActive(false);
+        this.ImageCongrats.SetActive(true);
+    }
+
+    protected override void AfterShown()
+    {
+        base.AfterShown();
+        SoundController.Instance.PlayOnce(this.FinishMusic);
+        this.CurrentScore.text = ScoreController.currentScore + "";
+        if (ScoreController.currentScore >= Data.MaxScore)
+        {
+            Data.MaxScore = ScoreController.currentScore;
+            this.ImageNewRecord.SetActive(true);
+        }
+        else
+        {
+            this.ImageNewRecord.SetActive(false);
+        }
+        this.MaxScore.text = Data.MaxScore + "";
+    }
+
+    public void BtnOKClick()
+    {
+        this.Hide();
+        SceneController.Instance.LoadHomeScene();
+    }
 }
