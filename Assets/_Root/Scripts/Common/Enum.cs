af66499097cd4c22e0b0c92ec408715b841853e3
dangthibaoan <baoan.ayako69@gmail.com>
2024-03-08 21:53:04 +0700

    add value cho enum để tránh sai sót khi sửa đổi

--- Assets/_Root/Scripts/Common/Enum.cs
+++ Assets/_Root/Scripts/Common/Enum.cs
@@ -1,32 +1,34 @@
 public enum SoundType
 {
-    ButtonClick,
-    BackgroundMusic,
-    BackgroundMusicOnGame,
-    Win,
-    Lose,
-    Walk,
-    TalkClick
+    ButtonClick = 0,
+    BackgroundMusic = 1,
+    BackgroundMusicOnGame = 2,
+    Win = 3,
+    Lose = 4,
+    Walk = 5,
+    TalkClick = 6,
 }
 public enum TriggerType
 {
-    NonType,
-    Player,
-    NonPlayer
+    NonType = 0,
+    Player = 1,
+    NonPlayer = 2,
 }
 public enum DialogType
 {
-    nonDialogType,
-    Talk,
-    Talk2
+    nonDialogType = 0,
+    Talk = 1,
+    Talk2 = 2,
 }
 public enum IDStoryLine
 {
-    nonStoryLine,
-    Story_Ludo,
-    Story_Luna
+    nonStoryLine = 0,
+    Story_Ludo = 1,
+    Story_Luna = 2,
 }
 public enum IDCharacter
 {
-    Usagi, Ludo, Luna
+    Usagi = 0,
+    Ludo = 1,
+    Luna = 2,
 }
